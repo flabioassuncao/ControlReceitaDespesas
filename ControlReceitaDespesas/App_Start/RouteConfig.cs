d869252e931794675cd295f27b552d0dbbffdddc
Flábio Assunção <flabiofilho@gmail.com>
2015-11-21 18:51:30 -0200

    Organização de Codigo
    Organização do codigo fonte das principais Views.

--- ControlReceitaDespesas/App_Start/RouteConfig.cs
+++ ControlReceitaDespesas/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ControlReceitaDespesas
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Controles", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
